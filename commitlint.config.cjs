cdb7459a16826e3e6a3738e20deef4fc49027206
jinho7 <69239293+jinho7@users.noreply.github.com>
2025-04-29 17:25:37 +0900

    chore: Add code style and commit conventions (#8)

---
+++ commitlint.config.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'subject-case': [
+            2,
+            'always',
+            [
+                'sentence-case',
+                'start-case',
+                'pascal-case',
+                'upper-case',
+                'lower-case',
+            ],
+        ],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'refactor',
+                'style',
+                'test',
+                'chore',
+                'hotfix',
+                'perf',
+                'ci'
+            ],
+        ],
+        'type-case': [2, 'always', 'lower-case'],
+        'subject-full-stop': [2, 'never', '.'],
+        'subject-min-length': [2, 'always', 5],
+        'header-max-length': [2, 'always', 72],
+        'scope-empty': [2, 'always'],
+        'body-leading-blank': [1, 'always'],
+        'footer-leading-blank': [1, 'always'],
+    },
+};
