9e8d35e84f4b1ce80b7aa27c463890e9978784a8
Dongkeun <hongdongk@naver.com>
2025-04-29 22:54:03 +0900

    feat: 회원가입 페이지 퍼블리싱 완료

--- src/app/auth/signup/page.tsx
+++
@@ -1,6 +0,0 @@
-import Image from "next/image";
-import style from "./page.module.css";
-
-export default function Page() {
-  return <div className={style.container}>회원가입 페이지</div>;
-}
--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
-import Link from "next/link";
-import style from "./MainHeader.module.css";
+import Image from 'next/image';
+import Link from 'next/link';
+import style from './MainHeader.module.css';
 
 export default function MainHeader() {
   return (
     <header className={style.header}>
-      <Link href={"/"}>
+      <Link href={'/'}>
         <div className={style.logo}>
-          <Image src={"/images/logo.svg"} width={30} height={47} alt={"로고"} />
+          <Image src={'/images/logo.svg'} width={30} height={50} alt={'로고'} />
           <p>Looper</p>
         </div>
       </Link>
