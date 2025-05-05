6f2b510cb901b835fa9758ccecba156f2fa689ca
Dongkeun <hongdongk@naver.com>
2025-05-05 19:24:27 +0900

    chore: login.tsx 변수숨김처리

--- src/app/(before-loginLayout)/auth/login/page.tsx
+++ src/app/(before-loginLayout)/auth/login/page.tsx
@@ -2,9 +2,7 @@ import Image from 'next/image';
 import style from './page.module.css';
 
 export default function Page() {
-  const KAKAO_REST_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_KEY;
-  const REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI || 'http://localhost:3000/auth/kakao/callback';
-  const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_REST_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
 
   return (
     <div className={style.container}>
