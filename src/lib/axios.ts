069cf9de594ed5d8384c7efa3818214cf1f85ea2
Dongkeun <hongdongk@naver.com>
2025-05-11 21:16:24 +0900

    test: 로그인문제 테스트

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -17,33 +17,33 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-// 403이면 자동 refresh토큰으로 갱신
-api.interceptors.response.use(
-  (res) => res,
-  async (error) => {
-    if (error.response?.status === 403) {
-      try {
-        const refreshRes = await axios.post(
-          `${process.env.NEXT_PUBLIC_API_SERVER_URL}/auth/token/refresh`,
-          {},
-          { withCredentials: true }
-        );
+// // 403이면 자동 refresh토큰으로 갱신
+// api.interceptors.response.use(
+//   (res) => res,
+//   async (error) => {
+//     if (error.response?.status === 403) {
+//       try {
+//         const refreshRes = await axios.post(
+//           `${process.env.NEXT_PUBLIC_API_SERVER_URL}/auth/token/refresh`,
+//           {},
+//           { withCredentials: true }
+//         );
 
-        const { accessToken, user } = refreshRes.data.data;
-        userStore.getState().setAccessToken(accessToken);
-        userStore.getState().setUser(user);
+//         const { accessToken, user } = refreshRes.data.data;
+//         userStore.getState().setAccessToken(accessToken);
+//         userStore.getState().setUser(user);
 
-        error.config.headers.Authorization = `Bearer ${accessToken}`;
-        return api.request(error.config);
-      } catch (err) {
-        userStore.getState().clearAuth();
-        // window.location.href = '/auth/login';
-        return Promise.reject(err);
-      }
-    }
+//         error.config.headers.Authorization = `Bearer ${accessToken}`;
+//         return api.request(error.config);
+//       } catch (err) {
+//         userStore.getState().clearAuth();
+//         // window.location.href = '/auth/login';
+//         return Promise.reject(err);
+//       }
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
 
 export default api;
