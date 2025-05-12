88be20c7fe0f79717fd3f89538c174d6bf62fc55
CoderDuck <hongdongk@naver.com>
2025-05-12 13:17:43 +0900

    chore: 회원가입, 회의록 추가 문구 변경

--- src/app/(before-loginLayout)/auth/signup/page.tsx
+++ src/app/(before-loginLayout)/auth/signup/page.tsx
@@ -1,10 +1,11 @@
-import style from "./page.module.css";
-import SignupForm from "@/components/form/SignupForm";
+import style from './page.module.css';
+import SignupForm from '@/components/form/SignupForm';
 
 export default function Page() {
   return (
     <div className={style.container}>
       <h1>Looper에서 사용할 닉네임을 입력해주세요</h1>
+      <h2>회의록에서 사용할 이름과 동일하게 입력해주세요</h2>
       <SignupForm />
     </div>
   );
