8b1ddfde0d4d3d373018c2f242ccabe98efdc6e7
CoderDuck <hongdongk@naver.com>
2025-05-13 14:41:36 +0900

    fix: 추천 스케줄 캐싱 제거

--- src/hooks/api/projects/useCreateMeetingNote.ts
+++ src/hooks/api/projects/useCreateMeetingNote.ts
@@ -1,9 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createProjectMeetingNote } from '@/api/project';
 import { AxiosError } from 'axios';
 
 export const useCreateMeetingNote = () => {
+  const queryClient = useQueryClient();
+
   return useMutation<void, AxiosError, { projectId: number; meetingNote: string }>({
     mutationFn: ({ projectId, meetingNote }) => createProjectMeetingNote(projectId, meetingNote),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['recommended-schedules'] }); // 생성 후 목록 다시 fetch
+    },
   });
 };
