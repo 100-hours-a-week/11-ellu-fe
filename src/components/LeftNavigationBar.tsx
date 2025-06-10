2df2bb5ee80f64a20570265feff55bc91a07887b
CoderDuck <hongdongk@naver.com>
2025-06-10 14:24:03 +0900

    feat: LeftNavigationBar 스타일 수정 및 챗봇섹션 추가

--- src/components/LeftNavigationBar.tsx
+++ src/components/LeftNavigationBar.tsx
@@ -5,13 +5,19 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import style from './LeftNaviagtionBar.module.css';
 
+import Diversity3Icon from '@mui/icons-material/Diversity3';
+import EditCalendarIcon from '@mui/icons-material/EditCalendar';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ChatIcon from '@mui/icons-material/Chat';
+
 export default function LeftNavigationBar() {
   const pathname = usePathname();
 
   const navItems = [
-    { label: '프로젝트', href: 'projects', icon: '/images/project.svg' },
-    { label: '캘린더', href: 'my-calendar', icon: '/images/calendar.svg' },
-    { label: '마이페이지', href: 'mypage', icon: '/images/mypage.svg' },
+    { label: '프로젝트', href: 'projects', icon: <Diversity3Icon /> },
+    { label: '캘린더', href: 'my-calendar', icon: <EditCalendarIcon /> },
+    { label: '마이페이지', href: 'mypage', icon: <AccountCircleIcon /> },
+    { label: '챗봇', href: 'chatbot', icon: <ChatIcon /> },
   ];
 
   return (
@@ -20,7 +26,7 @@ export default function LeftNavigationBar() {
         {navItems.map((item) => (
           <li key={item.href} className={pathname.split('/')[1] === item.href ? style.active : ''}>
             <Link href={`/${item.href}`}>
-              <Image src={item.icon} alt={item.label} width={25} height={25} className={style.icon} />
+              {item.icon}
               <span>{item.label}</span>
             </Link>
           </li>
