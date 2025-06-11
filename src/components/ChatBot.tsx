7e8ecd8e68cadfffcfa222b9b9ba4e3a7458bbbf
CoderDuck <hongdongk@naver.com>
2025-06-11 11:32:46 +0900

    feat: 챗봇 UI 구현완료

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -4,20 +4,74 @@ import { userStore } from '@/stores/userStore';
 import style from './ChatBot.module.css';
 import Image from 'next/image';
 import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+import { useState, useEffect, useRef } from 'react';
+
+interface Message {
+  content: string;
+  isUser: boolean;
+}
 
 export default function ChatBot() {
   const { user } = userStore();
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handleSubmit = () => {
+    if (!message.trim()) return;
+    // setMessages([...messages, { content: message, isUser: true }]);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { content: message, isUser: true },
+      { content: '저의 선택은요', isUser: false },
+    ]);
+    setMessage('');
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      if (e.nativeEvent.isComposing) return;
+      e.preventDefault();
+      e.stopPropagation();
+      handleSubmit();
+    }
+  };
 
   return (
     <div className={style.container}>
-      <div className={style.header}>
-        <Image src="/images/logo.svg" alt="로고" width={28} height={28} />
-        {user?.nickname}님, &nbsp;안녕하세요
-      </div>
+      {messages.length === 0 && (
+        <div className={style.header}>
+          <Image src="/images/logo.svg" alt="로고" width={28} height={28} />
+          {user?.nickname}님, &nbsp;안녕하세요
+        </div>
+      )}
       <div className={style.chatbot}>
+        {messages.length > 0 && (
+          <div className={style.messageBox}>
+            {messages.map((msg, index) =>
+              msg.isUser ? (
+                <div key={index} className={`${style.message} ${style.user_message}`}>
+                  {msg.content}
+                </div>
+              ) : (
+                <div key={index}>
+                  <Image src="/images/logo.svg" alt="로고" width={25} height={25} />
+                  <div key={index} className={`${style.message} ${style.bot_message}`}>
+                    {msg.content}
+                  </div>
+                </div>
+              )
+            )}
+          </div>
+        )}
         <div className={style.chatbot_input}>
-          <textarea placeholder="오늘은 어떤 일정을 계획중인가요?" className={style.chatbot_input_field} />
-          <button className={style.chatbot_input_button}>
+          <textarea
+            placeholder="오늘은 어떤 일정을 계획중인가요?"
+            className={style.chatbot_input_field}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <button className={style.chatbot_input_button} disabled={!message.trim()} onClick={handleSubmit}>
             <ArrowCircleUpIcon />
           </button>
         </div>
